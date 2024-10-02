54d4eae7d5e5d01a3a45d3030abc0aadce269b4e
Koketso-dax <lanchaster.k@gmail.com>
2024-10-02 20:25:14 +0200

    PEP8 validation

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,9 +3,11 @@
 Module for pascal triangle function.
 """
 
+
 def pascal_triangle(n):
     """
-    Returns a list of lists of integers representing the Pascal’s triangle of n.
+    Returns a list of lists of integers
+    representing the Pascal’s triangle of n.
     """
     if n <= 0:
         return []
