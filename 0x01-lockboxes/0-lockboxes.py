0dd1a2e2e66da48f3979e8c28d8f2498e283d1e5
Koketso-dax <lanchaster.k@gmail.com>
2024-10-08 09:37:05 +0200

    PEP 8 validation

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -17,7 +17,8 @@ def canUnlockAll(boxes: list) -> bool:
     Example:
         >>> canUnlockAll([[1], [2], [3], [4], []])
         True
-        >>> canUnlockAll([[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]])
+        >>> canUnlockAll([[1, 4, 6], [2], [0, 4, 1],
+                          [5, 6, 2], [3], [4, 1], [6]])
         True
         >>> canUnlockAll([[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]])
         False
