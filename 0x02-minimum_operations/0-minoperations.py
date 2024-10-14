ecdc913836de22b0738f8fad9a49ea46912fc536
Koketso-dax <lanchaster.k@gmail.com>
2024-10-14 22:18:11 +0200

    Solved minops problem

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" repeatedly dividing n by its smallest possible prime factor,
+which corresponds to the minimum number of operations needed
+to achieve that factor. """
+
+
+def minOperations(n: int) -> int:
+    """ Return min number of ops required to copy x paste. """
+    if n <= 1:
+        return 0
+    
+    ops:int = 0
+    i: int = 2
+    while i <= n:
+        if n % i == 0:
+            ops += i
+            n /= i
+        else:
+            i += 1
+    return ops
