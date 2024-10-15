1c396dbf3ee03899fbcd37042ef6a03581c6d0b1
Koketso-dax <lanchaster.k@gmail.com>
2024-10-15 19:20:14 +0200

    PEP8 validation

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,15 +6,19 @@ to achieve that factor. """
 
 def minOperations(n: int) -> int:
     """ Return min number of ops required to copy x paste. """
-    if n <= 1:
+    # all outputs should be at least 2 char: (min, Copy All => Paste)
+    if (n < 2):
         return 0
-
-    ops:int = 0
-    i: int = 2
-    while i <= n:
-        if n % i == 0:
-            ops += i
-            n /= i
-        else:
-            i += 1
+    ops, root = 0, 2
+    while root <= n:
+        # if n evenly divides by root
+        if n % root == 0:
+            # total even-divisions by root = total operations
+            ops += root
+            # set n to the remainder
+            n = n / root
+            # reduce root to find remaining smaller vals that evenly-divide n
+            root -= 1
+        # increment root until it evenly-divides n
+        root += 1
     return ops
