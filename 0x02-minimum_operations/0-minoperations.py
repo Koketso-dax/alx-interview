9ec2704f54810bbb3d836d53f16e5d4efdc37255
Koketso-dax <lanchaster.k@gmail.com>
2024-10-15 19:16:26 +0200

    mypy validation

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,13 +4,13 @@ which corresponds to the minimum number of operations needed
 to achieve that factor. """
 
 
-def minOperations(n: int) -> int:
+def minOperations(n):
     """ Return min number of ops required to copy x paste. """
     if n <= 1:
         return 0
     
-    ops:int = 0
-    i: int = 2
+    ops = 0
+    i = 2
     while i <= n:
         if n % i == 0:
             ops += i
