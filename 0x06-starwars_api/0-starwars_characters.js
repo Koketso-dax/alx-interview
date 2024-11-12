b7c4b23dc1b08d01e2245014ee9ab54855b42d93
Koketso-dax <lanchaster.k@gmail.com>
2024-11-12 15:33:53 +0200

    [Fix] Semistandard linting

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,17 +3,16 @@
 
 const request = require('request');
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2],
-    function (err, res, body) {
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
+});
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
     if (err) throw err;
-    const actors = JSON.parse(body).characters;
-    exactOrder(actors, 0);
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
   });
-  const exactOrder = (actors, x) => {
-    if (x === actors.length) return;
-    request(actors[x], function (err, res, body) {
-      if (err) throw err;
-      console.log(JSON.parse(body).name);
-      exactOrder(actors, x + 1);
-    });
-  };
+};
